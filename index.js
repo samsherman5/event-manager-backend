5267f030f39804edbb661344a512ff204228eb67
neoncodes <charleywolfofficial@gmail.com>
2023-07-10 13:20:09 -0400

    enabled body-parsers

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ const app = express();
   Middleware
 */
 
-// app.use(express.urlencoded({ extended: false }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(mongoSanitize());
 
