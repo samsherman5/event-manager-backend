67253f118b84646a374a957f2c2e02d1fe60840a
neoncodes <charleywolfofficial@gmail.com>
2023-07-10 13:30:40 -0400

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Imports
-const express = require('express');
 require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 
@@ -14,6 +15,7 @@ const app = express();
   Middleware
 */
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
